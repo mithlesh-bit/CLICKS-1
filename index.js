7a44c1d492880ae9e0e6efb2a2e158b721f88bdf
mithlesh-bit <rawtemithlesh52@gmail.com>
2024-03-05 00:18:23 +0530

    device storesdsdcccvvvvvvvvv

--- index.js
+++ index.js
@@ -55,13 +55,35 @@ console.log("page loaded");
         return 'Desktop';
     }
 
+    function getBrowserInfo() {
+        var ua = navigator.userAgent, tem,
+            M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return { name: 'IE', version: (tem[1] || '') };
+        }
+        if (M[1] === 'Chrome') {
+            tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+            if (tem != null) return { name: tem[1].replace('OPR', 'Opera'), version: tem[2] };
+        }
+        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+        if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+        return {
+            name: M[0],
+            version: M[1]
+        };
+    }
+
     function logInteraction(detail) {
         detail.userToken = getUserToken();
         detail.deviceType = getDeviceType();
         detail.adminID = config.adminID; // Include the admin ID in the detail
+        var browserInfo = getBrowserInfo();
+        detail.browserName = browserInfo.name; // Include browser name
+        detail.browserVersion = browserInfo.version; // Include browser version
 
         if (config.logConsole) {
-            console.log('Interaction logged:', detail);
+            console.log('Interaction logged:');
         }
         fetch(config.serverURL, {
             method: 'POST',
@@ -109,6 +131,7 @@ console.log("page loaded");
     window.TrackUserInteraction = {
         setConfig: function (userConfig) {
             Object.assign(config, userConfig);
+            config.adminID = getAdminId(); // Ensure admin ID is updated if config is reset
             attachEventListeners();
         }
     };
