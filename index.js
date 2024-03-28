dd81e1eaa74d879e246b6eeb1056ae7d2c876c9e
mithlesh-bit <rawtemithlesh52@gmail.com>
2024-03-28 08:25:02 +0530

    sdkfjn11122222333445566677

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // console.log(getDefaultSessionID());
     // console.log(getAllCookies());
     // console.log(getDeviceType());
-    console.log(getBrowserInfo());
+    // console.log(getBrowserInfo());
 
 
 
@@ -33,14 +33,15 @@ document.addEventListener('DOMContentLoaded', function () {
         return token || 'unidentifiedUser';
     }
 
-    function getAllCookies() {
-        const cookies = document.cookie.split('; ');
-        const cookiesObject = {};
-        cookies.forEach(cookie => {
-            const [name, value] = cookie.split('=');
-            cookiesObject[name] = value;
-        });
-        return cookiesObject;
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
     }
 
 
